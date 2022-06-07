89041142579464de0e39a465649e96e8ac5f4899
melleasalalima <melleasalalima@gmail.com>
2022-06-07 12:50:22 +0800

    11

--- customer_process.php
+++ customer_process.php
@@ -1,6 +1,6 @@
 <?php
 $search="";
 $rowcount=0;
-$con=new mysqli('localhost','root','','scm') or die (mysqli_error($mysqli));
+$con=new mysqli('localhost', 'u579272454_velen', 'Mamamopink123', 'u579272454_scm') or die (mysqli_error($mysqli));
 
 ?>
