3f675fc805603b14853240ff1abf073e68ca47cb
melleasalalima <melleasalalima@gmail.com>
2022-06-09 07:26:06 +0800

    Update admin_home.php

--- admin/admin_home.php
+++ admin/admin_home.php
@@ -73,44 +73,6 @@ if (isset($_GET['logout'])) {
         <p class="animated animate__bounce moving-text h-25">Welcome to Velen & Anna's Seafood Stalls! <i class="fa fa-star"></i>  We offer fresh seafood items</p>
     </header>
 
-    <?php
-      
-      $select_rows = mysqli_query($con, "SELECT * FROM `cart`") or die('query failed');
-      $row_count = mysqli_num_rows($select_rows);
-
-      ?>
-        <nav class="navbar navbar-expand-lg navbar-light bg-info p-4 sticky-top">
-            <a class="animate__animated animate__pulse animate__infinite navbar-brand display-4" href="home.php">Velen and Anna's Seafoods Dealer</a>
-
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-5">
-                    <!-- <li class="nav-item active">
-                        <a class="nav-link" href="#">Cart</span></a>
-                    </li> -->
-                    <li class="nav-item">
-                        <a class="nav-link" href="home.php"><i class="fa fa-home"></i></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="shop.php">Shop Now</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="cart.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i>Cart <span class="badge badge-light badge-pill"><?php echo $row_count; ?></span></a>
-                    </li>
-                </ul>
-                <!-- <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form> -->
-
-            </div>
-        </nav>
-        <p class="animated animate__bounce moving-text h-25">Welcome to Velen & Anna's Seafood Stalls! <i class="fa fa-star"></i>  We offer fresh seafood items</p>
-    </header>
-
 	<!-- Start ADMIN HOMEPAGE -->
 <div class="container">
 
