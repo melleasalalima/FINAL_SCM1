05706432277696793d4d7ccb01f1f86c42edd238
melleasalalima <melleasalalima@gmail.com>
2022-06-21 18:14:36 +0800

    Update home.php

--- home.php
+++ home.php
@@ -6,10 +6,10 @@
 <!-- Div for Brief Introduction -->
     <div class="container-fluid">
         <div class="shadow p-3 mb-3 mt-2 bg-body rounded">
-            <p class="animate__animated animate__fadeIn animate__delay-1s text-justify p-3 display-4">
+            <h1 class="animate__animated animate__fadeIn animate__delay-1s text-justify p-3">
                 Welcome to Velen and Ana’s Seafood Dealer! This is your one stop shop Fresh Seafood store right at the tip of your fingers.
                 We offer you a wide variety of seafood which you can order online and will be ready for pick-up at your preferred date and time.
-            </p>
+            </h1>
         </div>
     </div>
 
