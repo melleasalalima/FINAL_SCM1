510fce1a49d42e94b631034bba63545e4d6c93ae
melleasalalima <melleasalalima@gmail.com>
2022-06-21 20:55:00 +0800

    Update admin_index.php

--- admin/admin_index.php
+++ admin/admin_index.php
@@ -108,7 +108,7 @@
         <td><?php echo $row['lname'];?></td>
         <td><?php echo $row['address'];?></td>
         <td><?php echo $row['contact_num'];?></td>
-        <td>
+        <td class="text-align auto">
           <a href="admin_index.php?edit=<?php echo $row['id'];?>" class="btn btn-warning btn-sm">Edit</a>
           <a href="admin_process.php?delete=<?php echo $row['id'];?>" class="btn btn-danger btn-sm">Delete</a>
         </td>
