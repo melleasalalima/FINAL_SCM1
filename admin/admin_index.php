92e74a253c23a41007bcfa624710d978279ec966
melleasalalima <melleasalalima@gmail.com>
2022-06-21 21:01:33 +0800

    Update admin_index.php

--- admin/admin_index.php
+++ admin/admin_index.php
@@ -40,14 +40,96 @@
   <form action="admin_process.php" method="post">
     <?php if($update==true){?>
       <input type="hidden" name="id" value="<?php echo $id; ?>">
-      <label>Username: </label>&nbsp;&nbsp;&nbsp;<input type="text" name="username" required value="<?php echo $username; ?>"><br>
+
+      <div class="form-row">
+      <!-- Username -->
+          <div class="form-group col-md-6">
+            <label>Username</label>
+            <input type="text" name="username"class="form-control" required value="<?php echo $username; ?>">
+          </div>
+
+      <!-- Email -->
+          <div class="form-group col-md-6">
+          <label>Email</label>
+            <input type="email" name="email"  class="form-control" value="<?php echo $email; ?>">
+          </div>
+
+      <!-- User Type -->
+          <div class="form-group col-md-6">
+          <label>User Type: </label>
+          <select name="user_type" required>
+            <option value="<?php echo $user_type; ?>"><?php echo $user_type; ?></option>
+            <option value="admin">Admin</option>
+            <option value="staff">Staff</option>
+            <option value="customer">Customer</option>
+          </select><br>
+          </div>
+
+
+      <!-- First Name -->
+          <div class="form-group col-md-4">
+          <label>First Name</label>
+            <input type="text" name="fname" class="form-control" value="<?php echo $fname; ?>">
+          </div>
+
+
+      <!-- Middle Name -->
+          <div class="form-group col-md-4">
+          <label>Middle Name</label>
+            <input type="text" name="mname" class="form-control" placeholder="Optional" value="<?php echo $mname; ?>">
+          </div>
+
+      <!-- Last Name -->
+          <div class="form-group col-md-4">
+          <label>Last Name</label>
+            <input type="text" name="lname"  class="form-control"  value="<?php echo $lname; ?>">
+          </div>
+
+      <!-- Address -->
+          <div class="form-group col-md-4">
+          <label>Address</label>
+            <input type="text" name="address" class="form-control"  value="<?php echo $address; ?>">
+          </div>
+
+      <!-- Contact Number -->
+          <div class="form-group col-md-4">
+          <label>Contact Number</label>
+            <input type="text" name="contact_num"  class="form-control" value="<?php echo $contact_num; ?>">
+          </div>
+
+      <!-- User type -->
+        <div class="form-group col-md-4">
+          <label>User type</label>
+            <select name="user_type" id="user_type" class="form-control" >
+              <option value="admin">Admin</option>
+              <option value="staff">Staff</option>
+              <option value="customer" active>Customer</option>
+            </select>
+          </div>
+
+      <!-- password -->
+          <div class="form-group col-md-6">
+          <label>Password</label>
+            <input type="password"  class="form-control" name="password_1">
+          </div>
+
+          <div class="form-group col-md-6">
+          <label>Confirm password</label>
+            <input type="password"  class="form-control" name="password_2">
+          </div>
+
+          <div class="form-group">
+                    <button type="submit" class="btn btn-info mb-2" name="register_btn"> + Create user</button>
+          </div>
+        </div>
+        </form>
+
+      </div>
+
+      
+      
       <label>Email: </label>&nbsp;&nbsp;&nbsp;<input type="email" name="email" required value="<?php echo $email; ?>"><br>
-      <label>User Type: </label>&nbsp;&nbsp;&nbsp;<select name="user_type" required>
-        <option value="<?php echo $user_type; ?>"><?php echo $user_type; ?></option>
-        <option value="admin">Admin</option>
-        <option value="staff">Staff</option>
-        <option value="customer">Customer</option>
-      </select><br>
+
       <label>First Name: </label>&nbsp;&nbsp;&nbsp;<input type="text" name="fname" required value="<?php echo $fname; ?>"><br>
       <label>Middle Name: </label>&nbsp;&nbsp;&nbsp;<input type="text" name="mname" value="<?php echo $mname; ?>"><br>
       <label>Last Name: </label>&nbsp;&nbsp;&nbsp;<input type="text" name="lname" required value="<?php echo $lname; ?>"><br>
