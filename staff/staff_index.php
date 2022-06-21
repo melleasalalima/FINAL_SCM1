138a23aa64a636080361a78061e02f8135dbd818
melleasalalima <melleasalalima@gmail.com>
2022-06-21 20:09:47 +0800

    Update staff_index.php

--- staff/staff_index.php
+++ staff/staff_index.php
@@ -38,6 +38,8 @@
   </nav><br>
   <!-- End Search -->
 
+<div class="container">
+
 
 
   <!-- Input -->
@@ -97,7 +99,7 @@
   </div>
   </div>
   </div>
-
+  </div>
 <!-- Bootstrap -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.3/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
