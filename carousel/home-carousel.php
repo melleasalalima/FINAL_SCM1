f09288d14180047b3ba0440a6bce4abe9d1a9565
melleasalalima <melleasalalima@gmail.com>
2022-06-21 18:13:35 +0800

    Update home-carousel.php

--- carousel/home-carousel.php
+++ carousel/home-carousel.php
@@ -5,7 +5,7 @@
 
 <!-- IMG 1 -->
             <div class="carousel-item active">
-                <img src="images/gallery/img1.jpg" width="100%" height="900px">
+                <img src="images/gallery/img1.jpg" width="100%" height="600">
                 <div class="carousel-caption d-none d-md-block">
                     <div class="align-items-center justify-content-center mb-5">
                         <h1 class="animate__animated animate__bounce animate__infinite bg-info text-white shadow rounded">FRESH SEAFOOD STRAIGHT FROM THE SEA PORT</h1>
@@ -15,7 +15,7 @@
             </div>
 <!-- IMG 2 -->
             <div class="carousel-item">
-                <img src="images/gallery/img2.jpg" width="100%" height="900px">
+                <img src="images/gallery/img2.jpg" width="100%" height="600">
                 <div class="carousel-caption d-none d-md-block">
                     <div class="align-items-center justify-content-center mb-5">
                         <h1 class="animate__animated animate__bounce animate__infinite bg-info text-white shadow rounded">FRESH SEAFOOD RIGHT OFF THE BOAT</h1>
@@ -25,7 +25,7 @@
             </div>
 <!-- IMG 3 -->
             <div class="carousel-item">
-                <img src="images/gallery/img3.jpg" width="100%" height="900px">
+                <img src="images/gallery/img3.jpg" width="100%" height="600">
                 <div class="carousel-caption d-none d-md-block">
                     <div class="align-items-center justify-content-center mb-5">
                         <h1 class="animate__animated animate__bounce animate__infinite bg-info text-white shadow rounded">FRESH SEAFOOD RIGHT OFF THE BOAT</h1>
